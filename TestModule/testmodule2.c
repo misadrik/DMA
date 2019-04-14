bb1e3ec0b4d5d73de02507d0a4b79044a2d6ed43
MISaD <misad.chien@gmail.com>
2019-04-13 17:59:24 -0700

    only run kmalloc once

--- TestModule/testmodule2.c
+++ TestModule/testmodule2.c
@@ -9,32 +9,40 @@
 
 #define HELLO_MAJOR 231
 
+static unsigned char* buffer;
+
 int size = 0x1000;
 
+// static int my_simple_open(struct inode *inodep, struct file *filep)
+// {
+//     printk(KERN_INFO "MISaD: Device has been opened\n");
+//     filep->private_data = kzalloc(size, GFP_KERNEL);
+//     if (filep->private_data == NULL)
+//     {
+//         printk(KERN_ALERT "MISaD: Space allocte failed!\n");
+//         return -1;
+//     }
+//     memset(filep->private_data,'b',100);
+//     printk(KERN_INFO "MISaD: Space allocated %lx\n", filep->private_data);
+//     return 0;
+// }
 static int my_simple_open(struct inode *inodep, struct file *filep)
 {
     printk(KERN_INFO "MISaD: Device has been opened\n");
-    filep->private_data = kzalloc(size, GFP_KERNEL);
-    if (filep->private_data == NULL)
-    {
-        printk(KERN_ALERT "MISaD: Space allocte failed!\n");
-        return -1;
-    }
-    memset(filep->private_data,'b',100);
-    printk(KERN_INFO "MISaD: Space allocated %lx\n", filep->private_data);
     return 0;
 }
 
 static int my_simple_mmap(struct file *filp, struct vm_area_struct *vma)
 {
     printk(KERN_INFO "MISaD: Device mmap\n");
-    if (remap_pfn_range(vma, vma->vm_start, virt_to_pfn(filp->private_data), vma->vm_end - vma->vm_start, vma->vm_page_prot))
+    //if (remap_pfn_range(vma, vma->vm_start, virt_to_pfn(filp->private_data), vma->vm_end - vma->vm_start, vma->vm_page_prot))
+    if (remap_pfn_range(vma, vma->vm_start, virt_to_pfn(buffer), vma->vm_end - vma->vm_start, vma->vm_page_prot))
     {
         printk(KERN_ALERT "MISaD: Device mmap failed\n");
         return -EAGAIN;
     }
 
-    printk(KERN_INFO "MISaD: VIRT_ADDR %lx, PHY_ADDR %lx\n", vma->vm_start, virt_to_pfn(filp->private_data));
+    printk(KERN_INFO "MISaD: VIRT_ADDR %lx, PHY_ADDR %lx\n", vma->vm_start, virt_to_pfn(buffer));
     printk(KERN_INFO "MISaD: Device mmap OK\n");
     return 0;
 }
@@ -56,7 +64,7 @@ static struct file_operations fops = {
 static int __init testmodule_init(void)
 {
     int ret;
-    ret = register_chrdev(HELLO_MAJOR, "testmodule2", &fops);//old way of register a char device before 2.6 kernal
+    ret = register_chrdev(HELLO_MAJOR, "testmodule", &fops);//old way of register a char device before 2.6 kernal
     //void cdev_init(struct cdev *cdev, struct file_operactions *fops);
     //after structure setup, tell the kernel about it with a call
     //int cdev_add(struct cdev* dev, dev_t num, unsigned int count); //<0 fail
@@ -66,16 +74,25 @@ static int __init testmodule_init(void)
       printk(KERN_ALERT "MISaD: Can't register a module.\n");
       return ret;
     }
-    printk(KERN_INFO "MISaD: testmodule2 registerd.\n");
+    printk(KERN_INFO "MISaD: testmodule registerd.\n");
+    buffer = (unsigned char *)kzalloc(size, GFP_KERNEL);
+    SetPageReserved(virt_to_page(buffer));
+    memset(filep->private_data, 'b', 100);
+    printk(KERN_INFO "MISaD: Space allocated %lx\n", buffer);
+
     return 0;
 }
 
 static void __exit testmodule_exit(void)
 {
-  unregister_chrdev(HELLO_MAJOR, "testmodule2");
+  unregister_chrdev(HELLO_MAJOR, "testmodule");
   //remove a char device 
   //void cdev_del(struct cdev* dev);
-  printk(KERN_INFO "MISaD: testmodule2 removed.\n");
+  ClearPageReserved(virt_to_page(buffer));
+  printk(KERN_INFO "MISaD: PageReserved removed.\n");
+  kfree(buffer);
+  printk(KERN_INFO "MISaD: PageSpace released.\n");
+  printk(KERN_INFO "MISaD: testmodule removed.\n");
 }
 
 module_init(testmodule_init);
